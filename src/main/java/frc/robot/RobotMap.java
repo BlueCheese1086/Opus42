8745e949b2c2d954585ff8832d202c2a1aafdc84
ka2ki2 <59772200+ka2-ki2@users.noreply.github.com>
2022-02-18 19:29:20 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+public class RobotMap {
+    //this file contains all constants. if you need to add one, do it here. 
+    //currently all IDs are not set.
+
+    public final int FRONT_LEFT_ID = 0;
+    public final int FRONT_RIGHT_ID = 0;
+    public final int BACK_LEFT_ID = 0;
+    public final int BACK_RIGHT_ID = 0;
+
+    public final int CLIMB_LEFT_ID = 0;
+    public final int CLIMB_RIGHT_ID = 0;
+
+    public final int INDEXER_LEFT_ID = 0;
+    public final int INDEXER_RIGHT_ID = 0;
+
+    public final int INTAKE_MOTOR_ID = 0;
+    
+    
+    //todo - determine appropriate names for launcher falcons
+    public final int LAUNCHER_X_ID = 0;
+    public final int LAUNCHER_Y_ID = 0;
+
+    public final int PRIMARY_CONTROLLER_ID = 0;
+    public final int SECONDARY_CONTROLLER_ID = 0;
+}
