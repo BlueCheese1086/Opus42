7bd7a10e65b632500a360be091d737c715cf87f2
ka2ki2 <59772200+ka2-ki2@users.noreply.github.com>
2022-02-25 10:10:32 -0500

    Add files via upload

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,9 +2,12 @@ package frc.robot;
 
 public class Constants {
     public static final double LAUNCHER_DEFAULT_VELOCITY = 9.0; //meters/second
+   
 
     public static final double CAMERA_HEIGHT = 0.0; //meters, TODO: determine camera height
     public static final double CAMERA_ANGLE = 45; //degrees
 
     public static final double UPPER_HUB_HEIGHT = 2.64; //meters
+    public static final double GRAVITY = 9.8; //meters/second
+    public static final double CARGO_DIAMETER = 0.24;
 }
