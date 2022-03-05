52a50a35ae502ec8c5039460b774214f6a5a1621
Emsbry <59772305+Emsbry@users.noreply.github.com>
2022-03-05 15:03:25 -0500

    Update AutoTurn.java

--- src/main/java/frc/robot/autonomous/sections/AutoTurn.java
+++ src/main/java/frc/robot/autonomous/sections/AutoTurn.java
@@ -1,27 +1,39 @@
-
 package frc.robot.autonomous.sections;
 
-import frc.robot.components.Drivetrain;
+import frc.robot.Drivetrain;
 import frc.robot.Robot;
 import frc.robot.PIDController;
 import frc.robot.Constants;
 
 public class AutoTurn extends AutoSection{
 
-    Drivetrain drivetrain;
+    Drivetrian drivetrain;
     double current;
     double turningDist; // rotaions
     PIDController rTurnPIDController;
     PIDController lTurnPIDController;
+    boolean isAngleBased;
+    boolean isRight;
+    double power;
 
     /** angle = degrees */
-    public AutoTurn(double angle, Robot robot){
-        this.drivetrain = robot.drivetrain;
+    public AutoTurn(double angle){
+        this.drivetrain = Robot.drivetrain;
         this.turningDist = (((2 * Math.PI * Constants.WHEEL_TO_WHEEL_RADIUS) * (angle / 360 )) * 0.001) * Constants.DRIVETRAIN_POSITION_SCALE;
-        this.rTurnPIDController = new PIDController("angle", drivetrain.getFrontRight(), angle);
-        this.lTurnPIDController = new PIDController("angle", drivetrain.getFrontLeft(), angle);
-        lTurnPIDController.initPID(Constants.MP_DRIVE_FF, Constants.MP_DRIVE_KP, Constants.MP_DRIVE_KI, Constants.MP_DRIVE_KD);
-        rTurnPIDController.initPID(Constants.MP_DRIVE_FF, Constants.MP_DRIVE_KP, Constants.MP_DRIVE_KI, Constants.MP_DRIVE_KD);
+        this.rTurnPIDController = new PIDController("angle", drivetrain.frontRight, angle);
+        this.lTurnPIDController = new PIDController("angle", drivetrain.frontleft, angle);
+        drivetrain.frontLeft.initPID(Constants.MP_DRIVE_FF, Constants.MP_DRIVE_KP, Constants.MP_DRIVE_KI, Constants.MP_DRIVE_KD);
+        drivetrain.frontRight.initPID(Constants.MP_DRIVE_FF, Constants.MP_DRIVE_KP, Constants.MP_DRIVE_KI, Constants.MP_DRIVE_KD);
+        this.isAngleBased = true;
+
+    }
+
+    public AutoTurn(int length, boolean isRight, double power){
+        super(length);
+        this.drivetrain = Robot.drivetrain;
+        this.isAngleBased = false;
+        this.isRight = isRight;
+        this.power = power;
 
     }
 
@@ -29,15 +41,27 @@ public class AutoTurn extends AutoSection{
     public void init(){
         super.startTime = System.currentTimeMillis();
         super.started = true;
-        drivetrain.getFrontLeft().getEncoder().setPosition(0);
-        drivetrain.getFrontRight().getEncoder().setPosition(0);
+        if (isAngleBased){
+            drivetrian.frontLeft.getEncoder().setPosition(0);
+            drivetrain.frontRight.getEncoder().setPosition(0);
+        }
 
     }
 
     @Override
     public void update() {
-        rTurnPIDController.rotateToAngle(turningDist);
-        lTurnPIDController.rotateToAngle(turningDist * -1);
+        if (isAngleBased){
+            rturnPIDController.rotateToAngle(turningDist);
+            lturnPIDController.rotateToAngle(turningDist * -1);
+        } else {
+            if(isRight){
+                drivetrain.frontLeft.set(power);
+                drivetrain.frontRight.set(-power);
+            } else {
+                drivetrain.frontLeft.set(power);
+                drivetrain.frontRight.set(power);
+            }
+        }
 
     }
 
@@ -50,21 +74,6 @@ public class AutoTurn extends AutoSection{
 
     @Override
     public boolean disableCondition() {
-        if(turningDist - Constants.TURN_ERROR < drivetrain.getFrontRight().getEncoder().getPosition() &&  drivetrain.getFrontRight().getEncoder().getPosition() < turningDist + Constants.TURN_ERROR){
-            return true;
-        } else{
-            return false;
-        }
-    }
-
-    @Override
-    public double getStartTime(){
-        return super.getStartTime();
-    }
-
-    @Override
-    public double getEndTime(){
-        return super.getEndTime();
+        return turningDist - Constants.TURN_ERROR < drivetrain.frontRight.getEncoder().getPosition() < angle + Constants.TURN_ERROR : false;
     }
 }
-
