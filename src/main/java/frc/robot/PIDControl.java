ad0c294b18b361a79b502adf3df601bb081ff404
sesamesimile <59772305+Emsbry@users.noreply.github.com>
2022-03-12 16:51:06 -0500

    replace PIDController bc names are stupid

---
+++ src/main/java/frc/robot/PIDControl.java
@@ -0,0 +1,58 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+
+public class PIDControl {
+
+    String type;
+    double value;
+    CANSparkMax controller;
+    SparkMaxPIDController pid;
+
+    public PIDControl(String type, CANSparkMax controller, double value) {
+            this.type = type;
+            this.value = value;
+            this.controller = controller;
+    }
+
+    public PIDControl(String type, CANSparkMax controller) {
+        this.type = type;
+        this.controller = controller;
+}
+
+    public void initPID(double ff, double kp, double ki, double kd) {
+        pid = controller.getPIDController();
+        pid.setFF(ff);
+        pid.setP(kp);
+        pid.setI(ki);
+        pid.setD(kd);
+        pid.setOutputRange(-1, 1);
+    }
+
+    /** 
+     * turns the robot to the given angle
+     * @param angle = degrees
+     */
+    public void rotateToAngle(double angle) {
+        value = angle;
+        pid.setReference(angle, CANSparkMax.ControlType.kPosition, 0);
+    }
+
+    /** 
+     * move the robot the given distance
+     * @param distance = meters
+     */
+    public void driveDistance(double distance) {
+        value = distance;
+        pid.setReference(distance, CANSparkMax.ControlType.kPosition, 0);
+    }
+
+    public void setVelocity(double speed){
+        pid.setReference(speed, CANSparkMax.ControlType.kVelocity, 0);
+    }
+    
+}
+
